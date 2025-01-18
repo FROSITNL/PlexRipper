457a7a431ef425721c95fc6081d93247aa0b96e8
Fabio Ros <fabio@frosit.nl>
2025-01-18 18:00:25 +0000

    init standalone apps

---
+++ bin/README.md
@@ -0,0 +1,19 @@
+Standalone binaries
+===================
+
+Splits application in 2 parts, front and backend.
+No docker needed.
+Super fast.
+No headaches.
+usually...
+
+
+
+# Back-end
+
+wip
+
+
+# Front-end
+
+wip
---
+++ bin/build_webapp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# =====================
+# This script helps building executables for various platforms.
+#
+#
+# ===============
+
+echo "unfinished"
+
+exit 0
+# Install FNM
+curl -fsSL https://fnm.vercel.app/install | bash
+
+# Renew shell
+# @TODO
+
+# node
+fnm install 20
+
+# bun
+npm install -g bun
+
+# @TODO https://github.com/FROSITNL/addon-plexripper/blob/updates/plexripper/notes.md
